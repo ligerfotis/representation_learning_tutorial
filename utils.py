a72da0891999320bc39086daac90f050fad559c5
fotis <ligerfotis@gmail.com>
2023-09-27 11:41:30 +0200

    update simclr tutorial

--- utils.py
+++ utils.py
@@ -4,8 +4,6 @@ import torch
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
 
 
 def accuracy(output, target, topk=(1,)):
